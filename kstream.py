4a3b43664e948f364962608673262088ec5d05ba
Oleg Kats <kats@dev.kontur-extern.ru>
2009-12-24 15:24:35 +0500

    URL support in kstream ("kanso://master-location/path/to/file"). Some refactoring in get/read_structs.

--- kstream.py
+++ kstream.py
@@ -1,24 +1,44 @@
-import httplib, urllib 
+import httplib, urllib, string
+
+# TODO (kats): port
+def _parse_url(url):
+    scheme = netloc = path = query = "" 
+    # scheme
+    i = string.find(url, "://")
+    if i > 0:
+        scheme, url = string.lower(url[:i]), url[i+3:]
+    # netloc
+    i = string.find(url, "/")
+    if i > 0:
+        netloc, url = string.lower(url[:i]), url[i+1:]
+    # path and query
+    i = string.find(url, '?')
+    if i >= 0:
+        url, query = url[:i], url[i+1:]
+    
+    return scheme, netloc, url, query
 
 class kstream:
-	def __init__(self, filename):
-		self.off = 0
-		self.filename = filename
+    def __init__(self, filename):
+        self.off = 0
+        self.filename = filename
 
-	def get_next_chunk_loc(self):
-		#conn = httplib.HTTPConnection("kanso-master.kepref.kontur", 22222)
-		conn = httplib.HTTPConnection("localhost", 22222)
-		params = urllib.urlencode({"method":"read", "offset":self.off})
-		conn.request("GET", "/" + self.filename + "?" + params)
-		resp = str(conn.getresponse().read()).split(";")
-		return (resp[0], resp[1:])
+    def get_next_chunk_loc(self):
+        scheme, netloc, url, query = _parse_url(self.filename)
+        if scheme != "kanso": raise "wtf?"
+        conn = httplib.HTTPConnection(netloc, 22222)
+        params = urllib.urlencode({"method":"read", "offset":self.off})
+        conn.request("GET", "/" + url + "?" + params)
+        resp = str(conn.getresponse().read()).split(";")
+        return (resp[0], resp[1:])
 
-	def read_chunk(self, chunk_id, servers):
-		server = servers[0].split(":")
-		conn = httplib.HTTPConnection(server[0], server[1].split(",")[1])
-		conn.request("GET", "/" + chunk_id)
-		return conn.getresponse()
+    # TODO (kats): random chunkserver
+    def read_chunk(self, chunk_id, servers):
+        server = servers[0].split(":")
+        conn = httplib.HTTPConnection(server[0], server[1].split(",")[1])
+        conn.request("GET", "/" + chunk_id)
+        return conn.getresponse()
 
-	def read(self):
-		(chunk_id, servers) = self.get_next_chunk_loc()
-		return self.read_chunk(chunk_id, servers).read()
+    def read(self):
+        (chunk_id, servers) = self.get_next_chunk_loc()
+        return self.read_chunk(chunk_id, servers).read()
--- read_structs.py
+++ read_structs.py
@@ -6,7 +6,7 @@ TxnHead = namedtuple("TxnHead", "orgId dcId time type upfrCode accYear provId co
 Doc = namedtuple("Doc", "id type formKey fileName kansoOffset contentLen")
 Sign = namedtuple("Sign", "id docId type kansoOffset contentLen")
 
-def hash(bytes):
+def __hash(bytes):
 	h = 0x00000000
 	l = len(bytes)
 	for i in xrange(0, l-3, 4):
@@ -19,19 +19,19 @@ def hash(bytes):
 		h ^= unpack_from("=i", bytes+"\x00", l-3)[0]
 	return h
 
-def read_head(buf, off):
+def __read_head(buf, off):
 	layout = "=4x16s16sQix7siii"
 	t = unpack_from(layout, buf, off)
 	head = TxnHead(UUID(bytes_le=t[0]), UUID(bytes_le=t[1]), *t[2:8])
 	return head, off + calcsize(layout)
 
-def read_sign(bytes, off):
+def __read_sign(bytes, off):
 	layout = "=16s16siqi"
 	s = unpack_from(layout, bytes, off)
 	sign = Sign(UUID(bytes_le=s[0]), UUID(bytes_le=s[1]), *s[2:])
 	return sign, off+calcsize(layout)
 
-def read_doc(bytes, off):
+def __read_doc(bytes, off):
 	layout1 = "=16siiB"
 	d1 = unpack_from(layout1, bytes, off)
 	d2 = unpack_from("="+str(d1[3])+"s", bytes, off+calcsize(layout1))
@@ -39,7 +39,7 @@ def read_doc(bytes, off):
 	doc = Doc(UUID(bytes_le=d1[0]), d1[1], d1[2], d2[0], d3[0], d3[1])
 	return doc, off+calcsize(layout1)+calcsize("=qi")+d1[3]
 
-def read_array(buf, off, read_fn):
+def __read_array(buf, off, read_fn):
 	c = unpack_from("=i", buf, off)[0]
 	o = off + 4
 	elems = []
@@ -48,3 +48,28 @@ def read_array(buf, off, read_fn):
 		elems.append(elem)
 	return elems, o
 
+# TODO (kats): bounds checking
+def __read_txn(buf, off):
+	head, o = __read_head(buf, off)
+	docs, o = __read_array(buf, o, __read_doc)
+	signs, o = __read_array(buf, o, __read_sign)
+
+	if unpack_from("=i", buf, o)[0] == __hash(buf[off:o]):
+		off = o + 4
+		return (off, (head, docs, signs))
+
+__label = "===="
+def read_structs(buf, off=0):
+	o = off
+	while o < len(buf):
+		# this double-checking is slightly faster
+		if buf[o] != __label[0] or buf[o:o+4] != __label:
+			o += 1
+			continue
+
+		res = __read_txn(buf, o)
+		if not res: 
+			o += 1
+			continue
+		(o, txn) = res
+		yield res[1]

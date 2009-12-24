47a3d6af5070c88ff0d10e132b0a6a44bb5b17b9
Ibragim Youldachev <ib@gservice.ru>
2009-12-24 20:51:23 +0500

    Int conversion utility staff to process signed/unsigned int64 and int128 in Python SQLite API.

---
+++ intUtils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+__INT64MASK     = 0xFFFFFFFFFFFFFFFFL
+__INT64MAX      = 0x7FFFFFFFFFFFFFFFL
+
+def toSignedInt64(longInt):
+    if longInt > __INT64MAX:
+        return int((longInt & __INT64MAX ) - __INT64MAX - 1)
+    else: return longInt
+
+def toUnsignedInt64(int64):
+    if int64 < 0:
+        return __INT64MASK + int64 + 1
+    else: return int64
+
+def splitInt128(int128):
+    return toSignedInt64(int128 >> 64), toSignedInt64(int128 & __INT64MASK)
+
+def combineInt128(int64_hi, int64_low):
+    return (toUnsignedInt64(int64_hi) << 64) + toUnsignedInt64(int64_low)
+

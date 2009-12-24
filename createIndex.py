208925173e0899e23310b67138e56f973c1765f0
Ibragim Youldachev <ib@gservice.ru>
2009-12-24 20:57:11 +0500

    Index structure were updated and this script reflects that changes.

--- createIndex.py
+++ createIndex.py
@@ -8,8 +8,6 @@ from sqlite3 import connect
 connection = connect(__CONNECTION_STRING__)
 cursor = connection.cursor()
 cursor.execute('''drop table if exists state''')
-cursor.execute('''drop table if exists pfrSignatures''')
-cursor.execute('''drop table if exists pfrDocuments''')
 cursor.execute('''drop index if exists pfrTransactions_index''')
 cursor.execute('''drop table if exists pfrTransactions''')
 cursor.execute('''vacuum''')
@@ -29,33 +27,9 @@ cursor.execute('''
             upfrCode text,
             accountingYear integer,
             providerIdHash integer,
-            correctionType integer
-        )
-        ''')
-cursor.execute('''
-        create index pfrTransactions_index on pfrTransactions(orgId_hi, orgId_low)
-        ''')
-cursor.execute('''
-        create table pfrDocuments(
-            tr_id integer references pfrTransactions(id),
-            id_hi integer,
-            id_low integer,
-            type integer,
-            formKey integer,
-            fileName text,
-            kansoOffset integer,
-            contentLen integer
-        )
-        ''')
-cursor.execute('''
-        create table pfrSignatures(
-            tr_id integer references pfrTransactions(id),
-            id_hi integer,
-            id_low integer,
-            docId_hi integer,
-            docId_low integer,
-            contentLen integer,
-            kansoOffset integer
+            correctionType integer,
+            documents text,
+            signatures text
         )
         ''')
 

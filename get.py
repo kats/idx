33f68067b92cc3dd68a149c81a924158985eac98
Ibragim Youldachev <ib@gservice.ru>
2009-12-29 13:47:30 +0500

    Adds datetime.not() output to control execution time

--- get.py
+++ get.py
@@ -4,8 +4,13 @@ from kstream import *
 from read_structs import read_structs
 from IndexServer import *
 
+import datetime
+
+print datetime.datetime.now()
+
 updater = IndexServerUpdater("index.sqlite")
 for txn in read_structs(open("fake_kanso/chunks/4c44ed6d-4b46-4093-93f1-6b29a95e08ea", "rb").read()):
     updater.insert_record(txn)
 updater.commit()
 
+print datetime.datetime.now()

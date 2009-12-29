e6ab620ab7d9143a978f90246c918a5492dede93
Ibragim Youldachev <ib@gservice.ru>
2009-12-29 21:12:03 +0500

    Change timings output.

--- get.py
+++ get.py
@@ -6,11 +6,11 @@ from IndexServer import *
 
 import datetime
 
-print datetime.datetime.now()
+start = datetime.datetime.now()
 
 updater = IndexServerUpdater("index.sqlite")
 for txn in read_structs(open("fake_kanso/chunks/4c44ed6d-4b46-4093-93f1-6b29a95e08ea", "rb").read()):
     updater.insert_record(txn)
 updater.commit()
 
-print datetime.datetime.now()
+print str(datetime.datetime.now() - start)

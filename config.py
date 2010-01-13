4dfb1ef5aebba1ff50645bbc37e946d9c1c01d5e
Ibragim Youldachev <ib@gservice.ru>
2010-01-13 17:33:59 +0500

    More realistic delay

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ IDX_WEBSERVER_PORT = 8092
 # File URI with 'kanso://' namespace used to build index
 KANSO_FILENAME = "kanso://127.0.0.1/pfr/test/transactions"
 # Each time KANSO_FILENAME EOF reached we'll wait for KANSO_READ_UPDATES_DELAY secs
-KANSO_READ_UPDATES_DELAY = 10 # in secs
+KANSO_READ_UPDATES_DELAY = 60 # in secs
 # Index commited each time KANSO_FILENAME EOF reached and each IDX_ITEMS_PER_COMMIT.
 IDX_ITEMS_PER_COMMIT = 50000
 KANSO_MASTER_DEFAULT_PORT = 22222

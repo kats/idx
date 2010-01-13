ebc8d0a1e9832dd139f85abc93165fc5ed9f7e2b
Ibragim Youldachev <ib@gservice.ru>
2010-01-13 20:43:53 +0500

    Add logging to idxd.py

--- idxd.py
+++ idxd.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import config
+import logging
 
 from index import PfrIndex
 import searchserver
@@ -11,6 +12,8 @@ from collections import namedtuple
 from httplib import HTTPConnection
 from threading import Thread, Event
 from time import sleep
+
+log = logging.getLogger('idxd')
 
 def __start_search_daemon(port, events):
     t = Thread(target=searchserver.run, args=(port,events))
@@ -35,8 +38,10 @@ def __serve_forever(t_search, t_update, events):
         c.request('STOP', '')
     t_search.join(10)
     t_update.join(10)
+    log.info('finish')
 
 def run():
+    log.info('start')
     print 'Start serving idxd...'
     idx = PfrIndex(config.IDX_FILENAME)
     if not idx.validate() and \

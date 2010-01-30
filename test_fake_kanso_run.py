6a1d6bd1bc1436cbba0526df922717676528c0f0
Ibragim Youldachev <ib@gservice.ru>
2010-01-31 03:53:27 +0500

    Update to use new fake_kanso

--- test_fake_kanso_run.py
+++ test_fake_kanso_run.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from sqlite3 import connect
-from time import sleep
-
-import fake_kanso
-import config
-
-SLEEP = 30
+import fake_kanso, config
 
 if __name__ == '__main__':
+    fake_kanso.add_file('test_data/transactions', '/transactions')
+    fake_kanso.add_file('test_data/outgoingTransactions', '/outgoingTransactions')
     fake_kanso.run()
 
-    i = 0
-    while 1:
-        fake_kanso.append()
-        i += 1
-        print "Append #%d, sleep for %d secs" % (i, SLEEP)
-        sleep(SLEEP)
+    try:
+        while 1:
+            continue
+    except KeyboardInterrupt:
+        print "exit"

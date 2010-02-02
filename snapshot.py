2635c6f281a5de30d6d830957bad921f90135288
Ibragim Youldachev <ib@gservice.ru>
2010-02-02 17:20:04 +0500

    +copyfile

--- snapshot.py
+++ snapshot.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import config
+
 from datetime import datetime
 from os import listdir, remove, makedirs
 from os.path import join, isfile, isdir
 from re import compile, match
 from shutil import copyfile
+from time import time
+
+from index import PfrIndex
 
 def getFileSuffix(dt):
     return dt.strftime('%Y-%m-%d-%H-%M-%S-%f')
@@ -25,13 +30,33 @@ class Snapshot:
         if not isdir(self.__dstDir):
             makedirs(self.__dstDir)
         self.__snapshotFile = compile(self.__fileName.replace('.', '\.') + '\.\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}-\d{6}')
+        self.__t = int(time()) + config.IDX_SNAPSHOT_PERIOD
 
     def __dstFileName(self):
         return join(self.__dstDir, self.__fileName) + '.' + \
             getFileSuffix(datetime.now())
 
     def create(self):
-        copyfile(self.__fileName, self.__dstFileName())
+        dst = self.__dstFileName()
+        copyfile(self.__fileName, dst)
+        res = PfrIndex(dst).validate()
+        if not res:
+            self.remove(dst)
+        else:
+            self.__t = int(time())
+        self.__cleanup()
+        return res
+
+    def __cleanup(self):
+        names = listdir(self.__dstDir)
+        names.sort()
+        c = 0
+        for i in xrange(len(names)-1, -1, -1):
+            srcname = join(self.__dstDir, names[i])
+            if self.__snapshotFile.match(names[i]):
+                c += 1
+                if c > config.IDX_SNAPSHOT_MAX:
+                    self.remove(srcname)
 
     def latestFilePath(self):
         names = listdir(self.__dstDir)
@@ -39,7 +64,7 @@ class Snapshot:
         for i in xrange(len(names)-1, -1, -1):
             srcname = join(self.__dstDir, names[i])
             if self.__snapshotFile.match(names[i]):
-                return srcname; 
+                return srcname
 
     def replaceWithLatest(self):
         latest = self.latestFilePath()
@@ -51,4 +76,7 @@ class Snapshot:
     def remove(snapshotName):
         if isfile(snapshotName):
             remove(snapshotName)
+
+    def isTime(self):
+        return int(time()) > self.__t
 

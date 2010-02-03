019444b3a0fb70efa4954e14e607e3aa48fada94
Ibragim Youldachev <ib@gservice.ru>
2010-02-04 02:35:08 +0500

    +logging unexpected errors

--- idxd.py
+++ idxd.py
@@ -5,11 +5,13 @@ import config
 import logging
 
 from index import PfrIndex
+from snapshot import Snapshot
 import searchserver
 import indexserver
 
 from collections import namedtuple
 from httplib import HTTPConnection
+from sys import exc_info
 from threading import Thread, Event
 from time import sleep
 
@@ -44,8 +46,9 @@ def run():
     log.info('start')
     print 'Start serving idxd...'
     idx = PfrIndex(config.IDX_FILENAME)
+    snapshot_manager = Snapshot()
     if not idx.validate() and \
-            not (idx.restore() and idx.validate()) and \
+            not snapshot_manager.restore(PfrIndex) and \
             not idx.create():
         print "Please, close all connections to DB and try again."
         return -1
@@ -58,4 +61,8 @@ def run():
             events)
 
 if __name__ == '__main__':
-    run()
+    try:
+        run()
+    except:
+        log.critical('unexpected:%s' % str(exc_info()[1]))
+        raise

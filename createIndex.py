514e7d5867fc33d0c0e72185300368eeef61648c
Ibragim Youldachev <ib@gservice.ru>
2010-02-06 14:05:12 +0500

    Refactored

--- createIndex.py
+++ createIndex.py
@@ -3,42 +3,11 @@
 
 __CONNECTION_STRING = u'index.sqlite'
 
+from index import PfrIndex
 from os import remove
-import os.path
+from os.path import isfile
 
-if os.path.isfile(__CONNECTION_STRING):
-    remove(__CONNECTION_STRING)
-
-from sqlite3 import connect
-
-connection = connect(__CONNECTION_STRING)
-cursor = connection.cursor()
-#cursor.execute('PRAGMA page_size=32768;')
-
-cursor.execute('''
-        create table state(offset integer)
-        ''')
-cursor.execute('''
-        create table pfrTransactions(
-            id integer primary key autoincrement,
-            orgId_hi integer,
-            orgid_low integer,
-            dcId_hi integer,
-            dcId_low integer,
-            transactionTime integer,
-            transactionType integer,
-            upfrCode text,
-            accountingYear integer,
-            providerIdHash integer,
-            correctionType integer,
-            documents text,
-            signatures text
-        )
-        ''')
-
-cursor.execute('''
-        create index pfrTransactions_index on pfrTransactions(orgId_hi, orgId_low)
-        ''')
-
-connection.commit()
-connection.close()
+if __name__ == '__main__':  
+    if isfile(__CONNECTION_STRING):
+        remove(__CONNECTION_STRING)
+    idx = PrfIndex(__CONNECTION_STRING)
